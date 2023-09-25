cfeaa6e5ae8bfc395c7e2785f2bf011caf9e8339
Arvind Kumar <arvind7079573631@gmail.com>
2023-09-25 23:34:09 +0530

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("I am learning gihub actions");
+
+fetch("https://www.google.com/").then((resolve, error) => {
+  if (error) {
+    console.log(error);
+  } else console.log(resolve);
+});
